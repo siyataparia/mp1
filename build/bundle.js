7d32f5e2f3cc117660ea1f2692084f0eb50adee0
siyataparia <siyataparia@gmail.com>
2025-09-21 21:53:28 -0500

    fixed paths in index.js

---
+++ build/bundle.js
@@ -0,0 +1,42 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+__webpack_require__.r(__webpack_exports__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module './src/index.html'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+Object(function webpackMissingModule() { var e = new Error("Cannot find module './src/css/main.scss'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+Object(function webpackMissingModule() { var e = new Error("Cannot find module './src/js/main.js'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+/*
+ * This is the main entry point for Webpack, the compiler & dependency loader.
+ * All files that are necessary for your web page and need to be 'watched' for changes should be included here!
+ */
+// HTML Files
+//import './index.html';
+// Stylesheets
+//import './css/main.scss';
+// Scripts
+//import './js/main.js';
+
+
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
